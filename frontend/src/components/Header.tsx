dda2705620c864402dd84d70c4b7f3b0dc23e8ee
selastinmathew <selastinmathew@gmail.com>
2024-06-04 09:50:59 +0530

    mobile navigation has been done

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <nav className="border-b-2 border-b-orange-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to={"/"}>
+          <img src="./logo.png" alt="Logo" className="w-24 h-auto" />
+        </Link>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
