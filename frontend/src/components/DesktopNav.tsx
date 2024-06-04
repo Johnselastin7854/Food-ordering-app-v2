46ece8e71d9898530e6edb0f6729212c2d236237
selastinmathew <selastinmathew@gmail.com>
2024-06-04 09:59:13 +0530

    desktop navigation has been added

---
+++ frontend/src/components/DesktopNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const DesktopNav = () => {
+  return (
+    <Button
+      //   variant="ghost"
+      className="font-bold bg-orange-500 hover:bg-white hover:text-orange-500"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default DesktopNav;
