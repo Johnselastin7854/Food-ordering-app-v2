b8cb173a3614dd80d177009af66770e9b8a64dc5
selastinmathew <selastinmathew@gmail.com>
2024-06-11 22:16:30 +0530

    configure the cloudinary for image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,19 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import userRoute from "./routes/UserRoute";
+import { v2 as cloudinary } from "cloudinary";
 // import path from "path";
 // app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
