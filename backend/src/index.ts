c720839a7bc34cf563738498bf7ff83bd0939c1f
selastinmathew <selastinmathew@gmail.com>
2024-06-16 16:23:59 +0530

    get search api is created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoute from "./routes/UserRoute";
 import myRestaurantRoute from "./routes/MyRestaurant";
+import RestaurantRoute from "./routes/RestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 // import path from "path";
 // app.use(express.static(path.join(__dirname, "../../frontend/dist")));
@@ -22,13 +23,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/health", async (req: Request, res: Response) => {
-  res.send({ messgae: "Health is OK!" });
-});
+// app.get("/health", async (req: Request, res: Response) => {
+//   res.send({ messgae: "Health is OK!" });
+// });
 
 app.use("/api/user", userRoute);
 
 app.use("/api/my/restaurant", myRestaurantRoute);
+
+app.use("/api/restaurant", RestaurantRoute);
 
 app.listen(7000, () => {
   console.log(`Listening on POrt Number 7000`);
